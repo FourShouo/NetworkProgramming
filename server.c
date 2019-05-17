ed8de8734b4627c8d36186354327bfe309b2dea1
4422_花岡 四季 <k16157sh>
2019-05-17 10:30:59 +0900

    2019/05/17

---
+++ server.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define BUF_SIZE 256
+
+void commun(int);
+void DieWithError(char*);
+
+int main(int argc,char **argv){
+	int cliSock;
+	//実際の通信用ソケット
+	int servSock=socket(PF_INET,SOCK_STREAM,0);
+	//待ち受け専用ソケット
+	struct sockaddr_in servAddress;
+	struct sockaddr_in clientAddress;
+	unsigned int szClientAddr;
+	
+	servAddress.sin_family=AF_INET;
+	servAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+	servAddress.sin_port=htons(10001);
+	bind(servSock,(struct sockaddr *)&servAddress,sizeof(servAddress));
+	listen(servSock,5);  //順番待ちしてもいいクライアント数[二つ目の引数](実際はOSによって挙動が異なる)
+	//だからと言って5人同時接続できるわけではない
+	while(1){
+		szClientAddr=sizeof(clientAddress);
+		cliSock=accept(servSock,(struct sockaddr *)&clientAddress,&szClientAddr);
+		commun(cliSock);
+	}
+	
+	close(servSock);
+	
+	return 0;
+}
+
+void commun(int sock){
+	char buf[BUF_SIZE];
+	int len_r=recv(sock,buf,BUF_SIZE,0);
+	if(len_r<=0)DieWithError("recv()failed");
+	buf[len_r]='\0';
+	printf("%s\n",buf);
+	if(send(sock,buf,strlen(buf),0)!=strlen(buf))DieWithError("send() sent a massage of unexpected bytes");
+}
+
+void DieWithError(char *errorMessage){
+	perror(errorMessage);
+	exit(1);
+}
+
+
+
